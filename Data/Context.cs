faa65e77ca7ca7333029fef7030bca7379148e21
AJ <avjmail@gmail.com>
2020-05-27 06:21:20 +0300

    crud person

---
+++ Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Leome.Model;
+
+namespace Leome.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Leome.Model.Person> Person { get; set; }
+    }
+}
---
+++ Pages/People/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Leome.Data;
+using Leome.Model;
+
+namespace Leome.Pages.People
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Leome.Data.Context _context;
+
+        public IndexModel(Leome.Data.Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Person.ToListAsync();
+        }
+    }
+}
