a3339f2b97136b297fe72c85ccf6a4f4dd2687ee
AJ <avjmail@gmail.com>
2020-06-10 22:53:01 +0300

    algorithm

--- GetBest.cs
+++ GetBest.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Leome.Pages
@@ -11,7 +12,33 @@ namespace Leome.Pages
     {
         public async static Task<IList<Person>> Candidates(Job job, IQueryable<Person> people)
         {
-            return await people.Take(3).ToListAsync();
+            var personsWithScores = new List<(Person person, int score)>();
+
+            foreach (var person in people)
+            {
+                int personsScore = 0;
+                foreach (var personTag in person.PersonTags)
+                {
+                    var jobTag = job.JobTags.FirstOrDefault(x => x.TagID == personTag.TagID);
+                    if (jobTag != null)
+                    {
+                        personsScore += 1; // for now, use same even weights; todo: use jobTag.Weight, also jobTag.SkillLevel
+                    }
+                }
+                personsWithScores.Add((person, personsScore));
+            }
+
+            const int topPersonsCount = 3;
+            
+            var personsRating = personsWithScores.OrderByDescending(x => x.score);
+            var maxScore = personsRating.First().score;
+            var equalToMax = personsRating.Where(x => x.score == maxScore);
+
+            if (equalToMax.Count() > topPersonsCount)
+            {
+                return equalToMax.Select(x => x.person).ToList();
+            }
+            return personsRating.Take(topPersonsCount).Select(x => x.person).ToList();
         }
     }
 }
